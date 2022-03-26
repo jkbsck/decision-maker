6fef18f1fca94ea2155809195ff2a4d6a6c01fb1
Jakub Sučík <sucik.jakub@gmail.com>
2022-03-26 10:38:25 +0100

    Initial commit.

---
+++ index.js
@@ -0,0 +1,11 @@
+const btnGenerate = document.getElementById("btnGenerate");
+const resultName = document.getElementById("resultName");
+btnGenerate.addEventListener("click", (e) => {
+    e.defaultPrevented;
+    randomValue = Math.floor(Math.random() * 2);
+    if (randomValue === 0) {
+        resultName.innerHTML = 'fuck me';
+    } else {
+        resultName.innerHTML = 'JAKUBKO';
+    }
+});
